9840c19711ae87ba3e52fc822be6aa1bb8a3074b
cody-mar10 <ccmartin6@wisc.edu>
2023-08-02 14:13:03 -0500

    converted cross validator to a duck typed protocol

--- src/lightning_cv/split.py
+++ src/lightning_cv/split.py
@@ -1,13 +1,43 @@
 from __future__ import annotations
 
 from abc import ABCMeta, abstractmethod, abstractproperty
-from typing import Iterator, TypeVar
+from typing import Iterator, Protocol, overload, runtime_checkable
 
 import numpy as np
 from numpy.typing import NDArray
 
 Int64Array = NDArray[np.int64]
 CVIterator = Iterator[tuple[Int64Array, Int64Array]]
+
+
+@runtime_checkable
+class CrossValidator(Protocol):
+    @overload
+    def split(self) -> CVIterator:
+        ...
+
+    @overload
+    def split(self, X, y, groups) -> CVIterator:
+        ...
+
+    @overload
+    def get_n_splits(self) -> int:
+        ...
+
+    @overload
+    def get_n_splits(self, X, y, groups) -> CVIterator:
+        ...
+
+
+@runtime_checkable
+class GroupCrossValidator(CrossValidator, Protocol):
+    @property
+    def train_group_ids(self) -> list[int]:
+        ...
+
+    @property
+    def val_group_id(self) -> int:
+        ...
 
 
 class BaseCrossValidator(metaclass=ABCMeta):
@@ -34,11 +64,6 @@ class BaseGroupCrossValidator(BaseCrossValidator):
     @abstractproperty
     def val_group_id(self) -> int:
         ...
-
-
-CrossValidator = TypeVar("CrossValidator", bound=BaseCrossValidator)
-GroupCrossValidator = TypeVar("GroupCrossValidator", bound=BaseGroupCrossValidator)
-GenericCrossValidator = CrossValidator | GroupCrossValidator
 
 
 class ImbalancedLeaveOneGroupOut(BaseGroupCrossValidator):
