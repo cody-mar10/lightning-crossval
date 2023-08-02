6427555a94e8dabe599a17b9dafc4a3f0a521348
cody-mar10 <ccmartin6@wisc.edu>
2023-08-02 14:13:26 -0500

    converted ModelT and config into duck typed protocols

--- src/lightning_cv/module.py
+++ src/lightning_cv/module.py
@@ -1,11 +1,52 @@
 from __future__ import annotations
 
-from typing import Generic, Optional, TypeVar
+from typing import Any, Optional, Protocol, runtime_checkable
 from warnings import warn
 
 from lightning import Fabric, LightningModule
-from lightning.pytorch.utilities.types import STEP_OUTPUT
+from lightning.pytorch.utilities.types import STEP_OUTPUT, LRSchedulerTypeUnion
 from pydantic import BaseModel
+
+from ._typing import ConfigOptimizerOutput
+
+
+@runtime_checkable
+class ModelConfig(Protocol):
+    fabric: Optional[Fabric]
+
+
+@runtime_checkable
+class ModelT(Protocol):
+    fabric: Optional[Fabric]
+
+    def __init__(self, config: ModelConfig):
+        ...
+
+    def training_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        ...
+
+    def validation_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        ...
+
+    def configure_optimizers(self) -> ConfigOptimizerOutput:
+        ...
+
+    def lr_scheduler_step(self, scheduler: LRSchedulerTypeUnion, metric: Any | None):
+        ...
+
+    def train(self, mode: bool):
+        ...
+
+    def eval(self):
+        ...
+
+    @property
+    def estimated_steps(self) -> int:
+        ...
+
+    @estimated_steps.setter
+    def estimated_steps(self, value: int):
+        ...
 
 
 class BaseModelConfig(BaseModel):
@@ -15,11 +56,7 @@ class BaseModelConfig(BaseModel):
         arbitrary_types_allowed = True
 
 
-ModelConfig = TypeVar("ModelConfig", bound=BaseModelConfig)
-
-
-# TODO: this needs to be a mixin instead since the LightningModule must be inited first
-class CrossValModuleMixin(Generic[ModelConfig]):
+class CrossValModuleMixin:
     def __init__(self, config: ModelConfig):
         if config.fabric is None:
             raise ValueError("Config must have an instance of `lightning.Fabric`")
@@ -44,7 +81,7 @@ class CrossValModuleMixin(Generic[ModelConfig]):
         self._estimated_steps = value
 
 
-class CrossValModule(CrossValModuleMixin, LightningModule, Generic[ModelConfig]):
+class CrossValModule(CrossValModuleMixin, LightningModule):
     def __init__(self, config: ModelConfig):
         LightningModule.__init__(self)
         CrossValModuleMixin.__init__(self, config)
@@ -52,6 +89,3 @@ class CrossValModule(CrossValModuleMixin, LightningModule, Generic[ModelConfig])
     # change signature since validation_step is required
     def validation_step(self, *args, **kwargs) -> STEP_OUTPUT:
         ...
-
-
-ModelT = TypeVar("ModelT", bound=CrossValModule)
