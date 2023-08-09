36659affdf2eb23948d258714a1ba954fb267882
cody-mar10 <ccmartin6@wisc.edu>
2023-08-09 18:17:01 -0500

    more specific typing about datamodule and a config class

--- src/lightning_cv/typehints/data.py
+++ src/lightning_cv/typehints/data.py
@@ -1,15 +1,42 @@
 from __future__ import annotations
 
-from typing import Iterator, Literal, Protocol, runtime_checkable
+from dataclasses import Field
+from typing import (
+    Any,
+    ClassVar,
+    Iterator,
+    Literal,
+    Protocol,
+    overload,
+    runtime_checkable,
+)
 
+from pydantic import BaseModel
 from torch.utils.data import DataLoader
 
 Stage = Literal["fit", "test", "predict"]
 CVDataLoader = Iterator[tuple[DataLoader, DataLoader]]
 
 
+# this is the typeshed impl
+class DataclassInstance(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
+
+
 @runtime_checkable
 class CrossValDataModuleT(Protocol):
+    @overload
+    def __init__(self, config: BaseModel):
+        ...
+
+    @overload
+    def __init__(self, config: DataclassInstance):
+        ...
+
+    @overload
+    def __init__(self, **kwargs: Any):
+        ...
+
     def setup(self, stage: Stage):
         ...
 
