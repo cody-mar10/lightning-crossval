8520da48131fee4fa1929a65af20cb71e17a9952
cody-mar10 <ccmartin6@wisc.edu>
2025-01-07 12:46:59 -0600

    removed protocols and pydantic references

--- src/lightning_cv/typehints/data.py
+++ src/lightning_cv/typehints/data.py
@@ -1,44 +1,6 @@
-from __future__ import annotations
+from typing import Iterator, Literal
 
-from dataclasses import Field
-from typing import (
-    Any,
-    ClassVar,
-    Iterator,
-    Literal,
-    Protocol,
-    overload,
-    runtime_checkable,
-)
-
-from pydantic import BaseModel
 from torch.utils.data import DataLoader
 
 Stage = Literal["fit", "test", "predict"]
 CVDataLoader = Iterator[tuple[DataLoader, DataLoader]]
-
-
-# this is the typeshed impl
-class DataclassInstance(Protocol):
-    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
-
-
-@runtime_checkable
-class CrossValDataModuleT(Protocol):
-    @overload
-    def __init__(self, config: BaseModel):
-        ...
-
-    @overload
-    def __init__(self, config: DataclassInstance):
-        ...
-
-    @overload
-    def __init__(self, **kwargs: Any):
-        ...
-
-    def setup(self, stage: Stage):
-        ...
-
-    def train_val_dataloaders(self, **dataloader_kwargs) -> CVDataLoader:
-        ...
--- src/lightning_cv/typehints/module.py
+++ src/lightning_cv/typehints/module.py
@@ -1,48 +1,8 @@
-from __future__ import annotations
+from typing import TYPE_CHECKING, Any, Mapping, Union
 
-from typing import Any, Mapping, Optional, Protocol, runtime_checkable
+if TYPE_CHECKING:
+    from torch import Tensor
+    from torch.optim import Optimizer
 
-from lightning import Fabric
-from lightning.pytorch.utilities.types import STEP_OUTPUT, LRSchedulerTypeUnion
-from torch.optim import Optimizer
-
-ConfigOptimizerOutput = Optimizer | Mapping
-
-
-@runtime_checkable
-class ModelConfig(Protocol):
-    fabric: Optional[Fabric]
-
-
-@runtime_checkable
-class ModelT(Protocol):
-    fabric: Optional[Fabric]
-
-    def __init__(self, config: ModelConfig):
-        ...
-
-    def training_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
-        ...
-
-    def validation_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
-        ...
-
-    def configure_optimizers(self) -> ConfigOptimizerOutput:
-        ...
-
-    def lr_scheduler_step(self, scheduler: LRSchedulerTypeUnion, metric: Any | None):
-        ...
-
-    def train(self, mode: bool = True):
-        ...
-
-    def eval(self):
-        ...
-
-    @property
-    def estimated_steps(self) -> int:
-        ...
-
-    @estimated_steps.setter
-    def estimated_steps(self, value: int):
-        ...
+ConfigOptimizerOutput = Union["Optimizer", Mapping]
+ForwardOutputType = Union["Tensor", Mapping[str, Any]]
