94676eba319de2df049adf63aa576c6bf08bdc5f
cody-mar10 <ccmartin6@wisc.edu>
2025-01-07 12:57:29 -0600

    create an ABC for CV datamodules - this allows better type hinting and more clearly communicates expectations to users
    other:
    - make cv init kwargs optional
    - remove `SimpleTensorDataset` in favor of using `torch.utils.data.Subset`, which fulfills the exact same requirement
    - raise `TypeError` when calling `train_dataloader` and `val_dataloader`
    - provide both mixins as well as an actual class
    - add fn to check if input datamodule is a cross validation datamodule

--- src/lightning_cv/data.py
+++ src/lightning_cv/data.py
@@ -1,138 +1,73 @@
-from __future__ import annotations
-
+from abc import ABC, ABCMeta, abstractmethod
 from copy import copy
-from typing import Callable, Optional, Protocol, runtime_checkable
+from typing import Optional
 
-import torch
 from lightning import LightningDataModule
-from torch.utils.data import DataLoader, Dataset
+from torch import arange
+from torch.utils.data import DataLoader, Dataset, Subset
 
-from lightning_cv.typehints import (
-    CrossValidator,
-    CVDataLoader,
-    Int64Array,
-    KwargType,
-    Stage,
-)
+from lightning_cv.typehints import KwargType
+from lightning_cv.typehints.data import CVDataLoader, Stage
+from lightning_cv.typehints.split import CrossValidator
 
 
-class SimpleTensorDataset(Dataset):
-    def __init__(self, tensor: torch.Tensor) -> None:
-        super().__init__()
-        self.data = tensor
+class BaseCVDataModule(ABC, metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, *args, **kwargs) -> None: ...
 
-    def __len__(self) -> int:
-        return int(self.data.size(0))
-
-    def __getitem__(self, idx: int | list[int]) -> torch.Tensor:
-        return self.data[idx]
-
-    @staticmethod
-    def collate(batch: list[torch.Tensor]) -> torch.Tensor:
-        return torch.stack(batch)
-
-
-@runtime_checkable
-class CrossValDataModuleT(Protocol):
-    def setup(self, stage: Stage): ...
-
+    @abstractmethod
     def train_val_dataloaders(self, **dataloader_kwargs) -> CVDataLoader: ...
 
+    @abstractmethod
+    def setup(self, stage: Stage) -> None: ...
 
-class CrossValidationDataModule(LightningDataModule):
+
+class CrossValidationDataModuleMixin(BaseCVDataModule):
     def __init__(
         self,
         dataset: Dataset,
         batch_size: int,
         cross_validator: type[CrossValidator],
-        cross_validator_config: KwargType,
-        collate_fn: Optional[Callable] = None,
+        cross_validator_config: Optional[KwargType] = None,
+        dataset_size: Optional[int] = None,
+        dataloader_type: type[DataLoader] = DataLoader,
         # attr names
         y_attr: Optional[str] = None,
         group_attr: Optional[str] = None,
         **dataloader_kwargs,
     ) -> None:
-        super().__init__()
-
         self.dataset = dataset
-        self.dataset_size = len(dataset)  # type: ignore
+
+        # this is customizable if dataset can customize what an individual unit of data is
+
+        if dataset_size is None:
+            dataset_size = len(dataset)  # type: ignore
+
+        self.dataset_size = dataset_size
+
+        self.dataloader_type = dataloader_type
 
         # sklearn CV splitters take X, y, and group args to the .split method
         # since we cant know the structure of the data, we can just split this index tensor
-        self._dataset_idx = torch.arange(self.dataset_size)
+        self._dataset_idx = arange(self.dataset_size)
 
         self._dataset_y_attr = y_attr
         self._dataset_group_attr = group_attr
 
         self._validate_dataset()
 
-        ###########
-
         self.batch_size = batch_size
         self.dataloader_kwargs = dataloader_kwargs
         self._cross_validator = cross_validator
-        self._cross_validator_config = cross_validator_config
-        self.collate_fn = collate_fn
+        self._cross_validator_config = (
+            cross_validator_config if cross_validator_config is not None else {}
+        )
+        self.data_manager = self._cross_validator(**self._cross_validator_config)
 
-    def setup(self, stage: Stage):
-        if stage == "fit":
-            self.data_manager = self._cross_validator(**self._cross_validator_config)
-        else:
-            raise ValueError(
-                f"{stage=} not allowed with cross validation. Only the `fit` stage is "
-                "allowed."
-            )
-
-    # TODO: for sklearn compatibility, this may need to take args to .split
-    # def train_val_dataloaders(
-    #     self, *split_args, dataloader_kwargs: Optional[KwargType] = None, **split_kwargs
-    # ) -> CVDataLoader:
-    def train_val_dataloaders(self, **dataloader_kwargs) -> CVDataLoader:
-        # if dataloader_kwargs is None:
-        #     dataloader_kwargs = dict()
-
-        train_kwargs, val_kwargs = self._split_train_val_kwargs(**dataloader_kwargs)
-
-        # for train_idx, val_idx in self.data_manager.split(*split_args, **split_kwargs):
-        # for train_idx, val_idx in self.data_manager.split(self._dataset_idx)
-        # TODO: pass X, y, and group?
-
-        try:
-            splits = self.data_manager.split()
-        except TypeError:
-            # probably sklearn style splitters
-            X = self._dataset_idx
-            if self._dataset_y_attr is not None:
-                y = getattr(self.dataset, self._dataset_y_attr, None)
-            else:
-                y = None
-
-            if self._dataset_group_attr is not None:
-                groups = getattr(self.dataset, self._dataset_group_attr, None)
-            else:
-                groups = None
-
-            splits = self.data_manager.split(X, y, groups=groups)
-
-        for train_idx, val_idx in splits:
-            train_idx, val_idx = self._from_numpy(train_idx, val_idx)
-            train_idx_dataset = SimpleTensorDataset(train_idx)
-            val_idx_dataset = SimpleTensorDataset(val_idx)
-
-            train_loader = DataLoader(
-                dataset=train_idx_dataset,
-                batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
-                **train_kwargs,
-            )
-            val_loader = DataLoader(
-                dataset=val_idx_dataset,
-                batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
-                **val_kwargs,
-            )
-
-            yield train_loader, val_loader
+    def _overwrite_dataloader_kwargs(self, **kwargs) -> KwargType:
+        # overwrite the shared/global dataloader kwargs with kwargs specifically input
+        # and then return a copy
+        return self.dataloader_kwargs | kwargs
 
     def _split_train_val_kwargs(self, **kwargs) -> tuple[KwargType, KwargType]:
         train_kwargs = self._overwrite_dataloader_kwargs(**kwargs)
@@ -140,13 +75,42 @@ class CrossValidationDataModule(LightningDataModule):
         val_kwargs["shuffle"] = False
         return train_kwargs, val_kwargs
 
-    def _overwrite_dataloader_kwargs(self, **kwargs) -> KwargType:
-        # overwrite the shared/global dataloader kwargs with kwargs specifically input
-        # and then return a copy
-        return self.dataloader_kwargs | kwargs
+    def train_val_dataloaders(self, **dataloader_kwargs) -> CVDataLoader:
+        train_kwargs, val_kwargs = self._split_train_val_kwargs(**dataloader_kwargs)
 
-    def _from_numpy(self, *arrays: Int64Array) -> tuple[torch.Tensor, ...]:
-        return tuple(torch.from_numpy(array) for array in arrays)
+        try:
+            splits = self.data_manager.split()
+        except TypeError:
+            # probably sklearn style splitters that sometimes take arguments
+            X = self._dataset_idx
+            if self._dataset_y_attr is not None:  # pragma: no cover <- already tested
+                y = getattr(self.dataset, self._dataset_y_attr, None)
+            else:
+                y = None
+
+            if self._dataset_group_attr is not None:
+                groups = getattr(self.dataset, self._dataset_group_attr, None)
+            else:  # pragma: no cover <- already tested
+                groups = None
+
+            splits = self.data_manager.split(X, y, groups=groups)
+
+        for train_idx, val_idx in splits:
+            train_subset = Subset(self.dataset, train_idx.tolist())
+            val_subset = Subset(self.dataset, val_idx.tolist())
+
+            train_loader = self.dataloader_type(
+                dataset=train_subset,
+                batch_size=self.batch_size,
+                **train_kwargs,
+            )
+            val_loader = self.dataloader_type(
+                dataset=val_subset,
+                batch_size=self.batch_size,
+                **val_kwargs,
+            )
+
+            yield train_loader, val_loader
 
     def _validate_dataset(self):
         if self._dataset_y_attr is not None:
@@ -164,3 +128,57 @@ class CrossValidationDataModule(LightningDataModule):
                     "used for group cross validation strategies in which each fold is validated "
                     "with non-target group labels."
                 )
+
+    def train_dataloader(self, **dataloader_kwargs):
+        raise TypeError(
+            "A cross validation datamodule shouldn't need a training dataloader"
+        )
+
+    def val_dataloader(self, **dataloader_kwargs):
+        raise TypeError(
+            "A cross validation datamodule shouldn't need a validation dataloader"
+        )
+
+
+class CrossValidationDataModule(CrossValidationDataModuleMixin, LightningDataModule):
+    def __init__(
+        self,
+        dataset: Dataset,
+        batch_size: int,
+        cross_validator: type[CrossValidator],
+        cross_validator_config: KwargType,
+        dataset_size: Optional[int] = None,
+        dataloader_type: type[DataLoader] = DataLoader,
+        # attr names
+        y_attr: Optional[str] = None,
+        group_attr: Optional[str] = None,
+        **dataloader_kwargs,
+    ) -> None:
+        LightningDataModule.__init__(self)
+        CrossValidationDataModuleMixin.__init__(
+            self=self,
+            dataset=dataset,
+            batch_size=batch_size,
+            cross_validator=cross_validator,
+            cross_validator_config=cross_validator_config,
+            dataset_size=dataset_size,
+            dataloader_type=dataloader_type,
+            y_attr=y_attr,
+            group_attr=group_attr,
+            **dataloader_kwargs,
+        )
+
+    def setup(self, stage: Stage) -> None:
+        if stage != "fit":
+            raise ValueError(
+                f"Stage {stage} is not supported cross validation datamodules"
+            )
+
+
+CV_DATAMODULE_METHOD_NAME = "train_val_dataloaders"
+
+
+def is_cross_val_datamodule(obj) -> bool:
+    return isinstance(obj, LightningDataModule) and hasattr(
+        obj, CV_DATAMODULE_METHOD_NAME
+    )
