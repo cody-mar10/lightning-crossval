b70ebab687d72709e955d582d3471dc99cf0e2ad
cody-mar10 <ccmartin6@wisc.edu>
2025-01-07 12:31:24 -0600

    use forward references for type annotations - use util fn for removing old checkpoints - also no need to keep a str template for checkpoint file names since the trainer handles that

--- src/lightning_cv/callbacks/checkpoint.py
+++ src/lightning_cv/callbacks/checkpoint.py
@@ -1,10 +1,11 @@
-from __future__ import annotations
-
 from math import isinf, isnan
-from typing import Literal, Optional, cast
+from typing import TYPE_CHECKING, Literal, Optional, cast
 
-import lightning_cv as lcv
-from lightning_cv.callbacks import Callback
+from lightning_cv.callbacks.base import Callback
+from lightning_cv.callbacks.utils import remove_old_checkpoints
+
+if TYPE_CHECKING:
+    from lightning_cv.trainer import CrossValidationTrainer
 
 
 class ModelCheckpoint(Callback):
@@ -18,13 +19,13 @@ class ModelCheckpoint(Callback):
         monitor: str = "loss",
         mode: Literal["min", "max"] = "min",
     ):
-        self.filename = "epoch={epoch}-fold={fold}-val_loss={loss:.4f}.ckpt"
         self.save_last = save_last
         self.save_top_k = save_top_k
         self.every_n_epochs = every_n_epochs or 1
         self.monitor = monitor
         self.mode = mode
 
+        # map epoch to val metric being monitored
         self.best_k_models: dict[int, float] = dict()
 
         self.filter_fn = max if self.mode == "min" else min
@@ -39,7 +40,7 @@ class ModelCheckpoint(Callback):
             )
         return -1
 
-    def on_train_fold_end(self, trainer: "lcv.CrossValidationTrainer"):
+    def on_train_fold_end(self, trainer: "CrossValidationTrainer"):
         # need to check for this directly instead of using trainer property
         # if training stops during the first epoch, this attribute will not exist
         if hasattr(trainer, "_current_val_metrics"):
@@ -49,7 +50,7 @@ class ModelCheckpoint(Callback):
             if trainer.current_epoch % self.every_n_epochs == 0:
                 self._save_topk_ckpt(trainer, val_metric)
 
-    def _save_topk_ckpt(self, trainer: "lcv.CrossValidationTrainer", metric: float):
+    def _save_topk_ckpt(self, trainer: "CrossValidationTrainer", metric: float):
         if self.save_top_k == 0:
             return
 
@@ -67,11 +68,11 @@ class ModelCheckpoint(Callback):
 
         if len(self.best_k_models) == k + 1:
             del_epoch = kth_best_model_epoch
-            if del_epoch != -1:
+            if del_epoch != -1:  # pragma: no cover <- this is tested
                 self.best_k_models.pop(del_epoch)
                 glob = self.__checkpoint_glob__.format(epoch=del_epoch)
-                for file in trainer.checkpoint_dir.glob(glob):
-                    file.unlink()
+
+                remove_old_checkpoints(trainer.checkpoint_dir, glob)
 
             # if current epoch would be deleted immediately don't save it
             if del_epoch != trainer.current_epoch:
@@ -80,11 +81,14 @@ class ModelCheckpoint(Callback):
             # otherwise just save the checkpoint
             self.save(trainer)
 
-    def on_train_end(self, trainer: "lcv.CrossValidationTrainer"):
+    def on_train_end(self, trainer: "CrossValidationTrainer"):
         if self.save_last:
             self.save(trainer, suffix="last", include_val_loss=False)
 
-    def save(self, trainer: "lcv.CrossValidationTrainer", **kwargs):
+    def save(self, trainer: "CrossValidationTrainer", **kwargs):
+        if "include_val_loss" not in kwargs:
+            kwargs["include_val_loss"] = True
+
         if not trainer.should_stop:
             # don't save if trainer was told to stop from external signals
             trainer.save(trainer.fold_manager, **kwargs)
