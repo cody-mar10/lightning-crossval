79d1d0f253c9f7171790604c1d1a014f15c3a03a
cody-mar10 <ccmartin6@wisc.edu>
2025-01-07 12:43:48 -0600

    mirror changes to `config.py` `TunableType`s - also added more comments

--- src/lightning_cv/tuning/hyperparameters.py
+++ src/lightning_cv/tuning/hyperparameters.py
@@ -1,40 +1,33 @@
-from __future__ import annotations
-
 from collections import defaultdict
-from pathlib import Path
-from typing import Mapping
+from typing import Mapping, Union
 
 import optuna
 
-from lightning_cv.tuning.config import (
-    HparamConfig,
-    TunableCategorical,
-    TunableType,
-    load_config,
-)
+from lightning_cv.tuning.config import HparamConfig, TunableCategorical, TunableType
 from lightning_cv.tuning.utils import FrozenDict
+from lightning_cv.typehints import FilePath
 
-TrialedValue = int | float | str
-IntFloatStrDict = dict[str, TrialedValue | dict[str, TrialedValue]]
+TrialedValue = Union[int, float, str]
+IntFloatStrDict = dict[str, Union[TrialedValue, dict[str, TrialedValue]]]
 
 
 class HparamRegistry:
-    __suggest_fields__ = {"low", "high", "step", "log", "choices", "name"}
-
     def __init__(self, config: HparamConfig):
         self.config = config
         self.named_config = config.hparams_dict()
         self.uniq_forbidden_combos = config.unique_forbidden_combos()
 
     @classmethod
-    def from_file(cls, config_file: str | Path):
-        config = load_config(config_file)
+    def from_file(cls, config_file: FilePath):
+        config = HparamConfig.from_file(config_file)
         return cls(config)
 
     def _register(self, x: TunableType, trial: optuna.Trial, resample: bool = False):
         method = f"suggest_{x.suggest}"
-        method_kwargs = x.model_dump(include=self.__suggest_fields__)
+        method_kwargs = x.to_optuna_kwargs()
 
+        # for resampling trial value, such as for hparam combinations
+        # that are forbidden
         if x.name in trial.distributions and resample:
             # need to delete previously sampled value
             # otherwise this will just keep returning the same value
@@ -68,8 +61,8 @@ class HparamRegistry:
 
         # further, need to convert to the correct nesting for the model config
         for key, value in trial.params.items():
-            type_config = self.named_config[key]
-            parent = self.named_config[key].parent
+            tunable_type = self.named_config[key]
+            parent = tunable_type.parent
 
             if parent is not None:
                 # create a new subdict with the parent as the key
@@ -79,23 +72,32 @@ class HparamRegistry:
                 current = nested_hparams
 
             if (
-                isinstance(type_config, TunableCategorical)
-                and type_config.map is not None
+                isinstance(tunable_type, TunableCategorical)
+                and tunable_type.map is not None
             ):
-                value = type_config.map[value]
+                value = tunable_type.map[value]
 
             if isinstance(value, Mapping):
                 current.update(value)
             else:
                 current[key] = value
 
+        # TODO: we should note that there is a special key called `data`
+        # reserved for datamodule/dataloader/data processing hparams
+        # nothing really needs to change other than documentation
         return dict(nested_hparams)
 
     def register_hparams(self, trial: optuna.Trial):
+        # get a suggest value for each hparam once
         for hparam in self.config.hparams:
             self._register(hparam, trial, False)
 
+        # then check if any forbidden combinations exist, which need
+        # to be resampled
         self._resolve_forbidden_combinations(trial)
+
+        # then we need to build this into the correct nesting
+        # based on the parent field for each hparam
         return self._nest_hparams(trial)
 
 
