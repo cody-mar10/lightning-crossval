52b914327dcafeaf8cb0e7cec44df82cde36bf1e
cody-mar10 <ccmartin6@wisc.edu>
2025-01-07 12:32:36 -0600

    export public members

--- src/lightning_cv/tuning/__init__.py
+++ src/lightning_cv/tuning/__init__.py
@@ -1,4 +1,10 @@
-from lightning_cv.tuning import config
-from lightning_cv.tuning.callbacks import TrialPruning
+from lightning_cv.tuning.callbacks import OptunaHyperparameterLogger, TrialPruning
 from lightning_cv.tuning.hyperparameters import HparamRegistry
 from lightning_cv.tuning.tuner import Tuner
+
+__all__ = [
+    "TrialPruning",
+    "OptunaHyperparameterLogger",
+    "HparamRegistry",
+    "Tuner",
+]
