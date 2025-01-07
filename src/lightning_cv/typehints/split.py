b7f77c48ae35d999af383c7fdb106ac78431390c
cody-mar10 <ccmartin6@wisc.edu>
2025-01-07 12:48:21 -0600

    remove `runtime_checkable` protocols - simplify `CrossValidator` protocol to fit `scikit-learn` API

--- src/lightning_cv/typehints/split.py
+++ src/lightning_cv/typehints/split.py
@@ -1,29 +1,12 @@
-from __future__ import annotations
+from typing import Callable, Iterator, Protocol
 
-from typing import Callable, Iterator, Protocol, runtime_checkable
-
-import numpy as np
+from numpy import int64
 from numpy.typing import NDArray
 
-Int64Array = NDArray[np.int64]
+Int64Array = NDArray[int64]
 CVIterator = Iterator[tuple[Int64Array, Int64Array]]
 
-Type = Callable[..., CVIterator]
 
-
-@runtime_checkable
 class CrossValidator(Protocol):
     # ignore input types and just require to implement these methods
     split: Callable[..., CVIterator]
-    get_n_splits: Callable[..., int]
-
-
-@runtime_checkable
-class GroupCrossValidator(CrossValidator, Protocol):
-    @property
-    def train_group_ids(self) -> list[int]:
-        ...
-
-    @property
-    def val_group_id(self) -> int:
-        ...
